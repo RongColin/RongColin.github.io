dfc22eefacb55e4abe627ddd3b0747b831a10f03
RongColin <R_X_Peng@163.com>
2024-12-04 18:26:39 +0800

    Add files via upload

---
+++ assets/detail-GUVkwXFN.js
@@ -0,0 +1,9 @@
+import{d as u,v,a as g,t as f,c as p,b as r,w as l,e as s,j as o,u as n,h as a,o as i,F as w,f as x,i as y}from"./index-DT3jvW5d.js";const k={class:"bg-white min-h-screen"},B={class:"p-2"},C={class:"text-2xl"},b={class:"text-sm pt-1"},j={class:"text-sm pt-1"},F={class:"text-sm pt-1 whitespace-pre-wrap"},D=u({__name:"detail",setup(N){v(()=>{});const _=g({goods:{name:"参芪猴头菇排骨汤",images:["https://i3.meishichina.com/atta/recipe/2012/04/13/20120413131449848162820.jpg?x-oss-process=style/p800"],desc:"黄芪有补气固表、止汗脱毒、生肌、利尿、退肿之功效。用于治疗气虚乏力，中气下陷，久泻脱肛，便血崩漏，表虚自汗，糖尿病等。",content:"排骨：350克、猴头菇：5-6朵、盐：适量、胡椒粉：少许、党参：5克、黄芪：3克、葱段：3段、姜片：4片、山楂片：5片",step:`1、小块的排骨用清水泡去血水。
+2、配料准备好。
+3、排骨凉水下锅加入绍酒和姜丝焯水。
+4、焯水后洗净捞出。
+5、猴头菇提前泡发洗净。
+6、排骨放入汤锅煮开去掉浮沫，加入配料转小火慢慢煮一个半小时左右。
+7、放入猴头菇煮开转小火接着煮40分钟左右。
+8、加入盐和胡椒粉调味即可，喜欢香菜的可以撒点香菜。
+9、这款【参芪猴头菇排骨汤】就做好了！`}}),{goods:e}=f(_);return(V,t)=>{const d=a("van-image"),m=a("van-swipe-item"),h=a("van-swipe");return i(),p("div",k,[r(h,{autoplay:3e3,"lazy-render":"",height:"200"},{default:l(()=>[(i(!0),p(w,null,x(n(e).images,c=>(i(),y(m,{key:c},{default:l(()=>[r(d,{src:c,fit:"cover",position:"center",width:"375",height:"200"},null,8,["src"])]),_:2},1024))),128))]),_:1}),s("div",B,[s("h2",C,o(n(e).name),1),t[0]||(t[0]=s("h4",{class:"pt-2"},"功效",-1)),s("p",b,o(n(e).desc),1),t[1]||(t[1]=s("h4",{class:"pt-2"},"食材",-1)),s("p",j,o(n(e).content),1),t[2]||(t[2]=s("h4",{class:"pt-2"},"步骤",-1)),s("p",F,o(n(e).step),1)])])}}});export{D as default};
---
+++ assets/index-DdbStNi_.js
@@ -0,0 +1,17 @@
+import{T as w,_ as k}from"./index-DuIq3z2v.js";import{d as C,r as K,a as L,t as V,c as s,b as n,e as a,w as B,F as o,f as p,u,g as T,s as U,h as i,o as t,i as d}from"./index-DT3jvW5d.js";const j={class:"flex w-full"},F={class:"flex-1 sidebar overflow-y-auto"},A=C({__name:"index",setup(N){const l=K(0),m=T(),_=L({soupList:[{name:"参芪猴头菇排骨汤",picUrl:"https://i3.meishichina.com/atta/recipe/2012/04/13/20120413131449848162820.jpg?x-oss-process=style/p800",efficacy:"补气固表、止汗脱毒、生肌、利尿、退肿之功效。",desc:"黄芪有补气固表、止汗脱毒、生肌、利尿、退肿之功效。用于治疗气虚乏力，中气下陷，久泻脱肛，便血崩漏，表虚自汗，糖尿病等。",content:"排骨：350克、猴头菇：5-6朵、盐：适量、胡椒粉：少许、党参：5克、黄芪：3克、葱段：3段、姜片：4片、山楂片：5片",step:`1、小块的排骨用清水泡去血水。
+2、配料准备好。
+3、排骨凉水下锅加入绍酒和姜丝焯水。
+4、焯水后洗净捞出。
+5、猴头菇提前泡发洗净。
+6、排骨放入汤锅煮开去掉浮沫，加入配料转小火慢慢煮一个半小时左右。
+7、放入猴头菇煮开转小火接着煮40分钟左右。
+8、加入盐和胡椒粉调味即可，喜欢香菜的可以撒点香菜。
+9、这款【参芪猴头菇排骨汤】就做好了！`},{name:"参芪猴头菇排骨汤",picUrl:"https://i3.meishichina.com/atta/recipe/2012/04/13/20120413131449848162820.jpg?x-oss-process=style/p800",efficacy:"补气固表、止汗脱毒、生肌、利尿、退肿之功效。",desc:"黄芪有补气固表、止汗脱毒、生肌、利尿、退肿之功效。用于治疗气虚乏力，中气下陷，久泻脱肛，便血崩漏，表虚自汗，糖尿病等。",content:"排骨(350克）、猴头菇(5-6朵)、盐(适量)、胡椒粉(少许)、党参(5克)、黄芪(3克)、葱段(3段)、姜片(4片)、山楂片(5片)",step:`1、小块的排骨用清水泡去血水。
+2、配料准备好。
+3、排骨凉水下锅加入绍酒和姜丝焯水。
+4、焯水后洗净捞出。
+5、猴头菇提前泡发洗净。
+6、排骨放入汤锅煮开去掉浮沫，加入配料转小火慢慢煮一个半小时左右。
+7、放入猴头菇煮开转小火接着煮40分钟左右。
+8、加入盐和胡椒粉调味即可，喜欢香菜的可以撒点香菜。
+9、这款【参芪猴头菇排骨汤】就做好了！`}],cateList:[{title:"热门推荐",paneKey:"2093"},{title:"清热解毒",paneKey:"2094"},{title:"补品",paneKey:"2095"},{title:"祛湿",paneKey:"2096"}]}),f=c=>U(c+"11"),h=()=>{m.push({path:"/other/detail",query:{id:"1"}})},{cateList:v,soupList:y}=V(_);return(c,r)=>{const b=i("van-notice-bar"),x=i("van-sidebar-item"),g=i("van-sidebar");return t(),s(o,null,[n(b,{"left-icon":"volume-o",scrollable:"",text:"靓汤推荐，美味实在"}),a("main",j,[a("div",null,[n(g,{modelValue:l.value,"onUpdate:modelValue":r[0]||(r[0]=e=>l.value=e),class:"sidebar overflow-y-auto",onChange:f},{default:B(()=>[(t(!0),s(o,null,p(u(v),e=>(t(),d(x,{key:e.paneKey,title:e.title},null,8,["title"]))),128))]),_:1},8,["modelValue"])]),a("div",F,[(t(!0),s(o,null,p(u(y),e=>(t(),d(k,{goods:e,onClick:h},null,8,["goods"]))),256))])]),n(w,{style:{height:"50px"}})],64)}}});export{A as default};
